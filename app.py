2ac369d0b33cc6755899ec715a24a16982abb6b0
saadahmad-1 <saadahmad.cs@gmail.com>
2024-10-02 15:01:20 +0500

    minor changes

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def get_playlist():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
